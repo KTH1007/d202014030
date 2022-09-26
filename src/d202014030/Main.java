a94a0de588f08140c08d39ee70c96604c2325111
KTH1007 <rlaxogjs2684@gmail.com>
2022-09-26 15:13:20 +0900

    Main.java 수정 #1

--- src/d202014030/Main.java
+++ src/d202014030/Main.java
@@ -2,6 +2,8 @@ package d202014030;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+	String s = "hello world";
+	System.out.println(s);
 	}
-}
+	}
+	
